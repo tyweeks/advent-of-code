21680796fffbf8c007757360f86df6834fe5a9cc
Ty Weeks <ty.weeks@codetweeks.com>
2025-12-02 21:42:25 -0800

    Day 03 solution

--- AdventOfCode2025.Tests/Day03Tests.cs
+++ AdventOfCode2025.Tests/Day03Tests.cs
@@ -7,7 +7,7 @@ public class Day03Tests
     private readonly Day03 _problem = new();
 
     [Theory]
-    [InlineData("", "")]
+    [InlineData("987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111", "357")]
     public void SolvePart1(string input, string expected)
     {
         var result = _problem.SolvePart1(input);
@@ -15,7 +15,22 @@ public class Day03Tests
     }
 
     [Theory]
-    [InlineData("", "")]
+    [InlineData("987654321111111", 2, "98")]
+    [InlineData("811111111111119", 2, "89")]
+    [InlineData("234234234234278", 2, "78")]
+    [InlineData("818181911112111", 2, "92")]
+    [InlineData("987654321111111", 12, "987654321111")]
+    [InlineData("811111111111119", 12, "811111111119")]
+    [InlineData("234234234234278", 12, "434234234278")]
+    [InlineData("818181911112111", 12, "888911112111")]
+    public void GetLargestJoltage(string input, int numBatteries, string largestJoltage)
+    {
+        var result = Day03.GetLargestJoltage(input.ToCharArray(), numBatteries);
+        Assert.Equal(largestJoltage, result);
+    }
+
+    [Theory]
+    [InlineData("987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111", "3121910778619")]
     public void SolvePart2(string input, string expected)
     {
         var result = _problem.SolvePart2(input);
--- AdventOfCode2025/Days/Day03.cs
+++ AdventOfCode2025/Days/Day03.cs
@@ -6,11 +6,57 @@ public class Day03 : IProblem
 {
     public string SolvePart1(string input)
     {
-        return "";
+        var inputs = ParseInput(input);
+        long sum = 0;
+
+        foreach (var bank in inputs)
+        {
+            sum += long.Parse(GetLargestJoltage(bank.ToCharArray(), 2));
+        }
+
+        return sum.ToString();
+    }
+
+    public static string GetLargestJoltage(char[] input, int batteries)
+    {
+        if (batteries == 0)
+            return "";
+
+        var maxJoltage = 0;
+        var maxIndex = 0;
+
+        for (int i = 0; i <= input.Length - batteries; i++)
+        {
+            var joltage = int.Parse(input[i].ToString());
+            if (joltage > maxJoltage)
+            {
+                maxJoltage = joltage;
+                maxIndex = i;
+            }
+        }
+
+        var maxString = input[maxIndex].ToString();
+        return maxString + GetLargestJoltage(input[(maxIndex+1)..], batteries-1);
     }
 
     public string SolvePart2(string input)
     {
-        return "";
+        var inputs = ParseInput(input);
+        long sum = 0;
+
+        foreach (var bank in inputs)
+        {
+            sum += long.Parse(GetLargestJoltage(bank.ToCharArray(), 12));
+        }
+
+        return sum.ToString();
+    }
+
+    private static List<string> ParseInput(string input)
+    {
+        return input
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Trim())
+            .ToList();
     }
 }
