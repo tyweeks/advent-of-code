3f18c865028d77d2864c1eb16d625a82af362b0f
Ty Weeks <ty.weeks@codetweeks.com>
2025-12-08 20:50:19 -0800

    day 08 setup

---
+++ AdventOfCode.Tests/Year2025/Day08Tests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Solutions.Year2025;
+
+namespace AdventOfCode.Tests.Year2025;
+
+public class Day08Tests
+{
+    private readonly Day08 _problem = new();
+
+    [Theory]
+    [InlineData("", "")]
+    public void SolvePart1(string input, string expected)
+    {
+        var result = _problem.SolvePart1(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("", "")]
+    public void SolvePart2(string input, string expected)
+    {
+        var result = _problem.SolvePart2(input);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AdventOfCode/Solutions/Year2025/Day08.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Solutions.Year2025;
+
+public class Day08 : ISolution
+{
+    public string SolvePart1(string input)
+    {
+        return "";
+    }
+
+    public string SolvePart2(string input)
+    {
+        return "";
+    }
+}
